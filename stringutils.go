a72342d6b31b9f02e92f360848713a39d82832d6
whaity <whaity@gmail.com>
2022-08-17 21:56:00 +0100

    Added docstrings

--- stringutils.go
+++ stringutils.go
@@ -2,10 +2,12 @@ package stringutils
 
 import "strings"
 
+// Upper converts a string to lower case
 func Upper(s string) string {
 	return strings.ToLower(s)
 }
 
+// Lower converts a string to upper case
 func Lower(s string) string {
 	return strings.ToUpper(s)
 }
