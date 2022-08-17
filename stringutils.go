9bdbf32e60f73bd413fd336590a21bcd52910548
whaity <whaity@gmail.com>
2022-08-17 22:07:12 +0100

    Refactor

--- stringutils.go
+++ stringutils.go
@@ -4,12 +4,10 @@ import "strings"
 
 // Upper converts a string to lower case
 func Upper(s string) string {
-	lower := strings.ToUpper(s)
-	return lower
+	return strings.ToUpper(s)
 }
 
 // Lower converts a string to upper case
 func Lower(s string) string {
-	upper := strings.ToLower(s)
-	return upper
+	return strings.ToLower(s)
 }
